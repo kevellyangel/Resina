8a1b994e91b9aae5caaeeb2fa21af2dba9dcf2b4
kevellyangel <marxcamila00@gmail.com>
2024-12-13 17:36:08 -0300

    Commit automático - Fri, Dec 13, 2024  5:36:08 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';  // Adicionando o import do Header
 import Home from './pages/Home';
 import Store from './pages/Store';
 import Cart from './components/Cart';
@@ -8,6 +9,7 @@ import Contact from './pages/Contact'; // Importando o componente de contato
 const App = () => {
   return (
     <Router>
+      <Header /> {/* Colocando o Header antes das rotas */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
